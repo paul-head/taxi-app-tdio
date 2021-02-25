3d839a2b92bbdf3de822c2809ef117b305c59348
Pavel Golovanov <publichead@yandex.ru>
2021-02-25 16:13:06 +0300

    add sign up view on url /api/sign_up * add User serialiser * add authentication test(user can sign up)

--- server/taxi/taxi/urls.py
+++ server/taxi/taxi/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from trips.views import SignUpView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/sing_up/', SignUpView.as_view(), name='sing_up'),
 ]
